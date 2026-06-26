9a7b456bbc16fa6add22430e2c52662713177d3a
Himani Lal <himani1032006@gmail.com>
2026-06-26 12:14:48 +0530

    ques done

---
+++ 2employee salary ques.py
@@ -0,0 +1,6 @@
+Attendence= float(input("enter the attendence percentage"))
+Rating=float(input("enter the rating"))
+if Attendence>75 and Rating>4:
+    print("Elligible for bonus")
+else:
+    print("Not elligible for bonus")
