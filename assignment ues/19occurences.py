94b1160163157f874bca6a46817b7f59feb6f3ec
Himani Lal <himani1032006@gmail.com>
2026-07-15 23:42:16 +0530

    one ans is changed

--- assignment ues/19occurences.py
+++ assignment ues/19occurences.py
@@ -1,9 +1,8 @@
 s="aaabcde"
 ch={}
 for i in s:
-    for i in ch:
-        if i in ch:
-            ch[i]=ch[i]+1
-        else:
-            ch[i]=ch[i]
-print(ch[i])
+     if i in ch:
+        ch[i]=ch[i]+1
+     else:
+        ch[i]=1
+print(ch)
